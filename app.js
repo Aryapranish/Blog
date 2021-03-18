91a48f6b2953c3a20ea464840ec0ea49e92b50ce
Arya Pranish Silwal <aryapranishsilwal@Aryas-MacBook-Pro.local>
2021-03-18 01:39:30 -0400

    Added express routing for easy page loads

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -21,6 +22,7 @@ app.use(express.static("public"));
 app.get("/",function (req,res) {
 
   // console.log(posts);
+  // console.log(posts);
   res.render('home.ejs', {startingContent: homeStartingContent, posts: posts});
 });
 
@@ -37,11 +39,25 @@ app.get("/compose",function (req,res) {
   res.render("compose")
 })
 
+
+//using express parameters
+app.get("/posts/:postID",function(req,res){
+  const postTitle = _.lowerCase(req.params.postID);
+  
+  posts.forEach(post => {
+    const storedTitle = _.lowerCase(post.title);
+    if (storedTitle === postTitle) {
+      res.render("post.ejs",{title: post.title, postContent: post.content})
+    }
+    else
+    {
+      console.log("Not a match");
+    }
+  });
+})
+
+
 app.post("/compose",function (req,res) {
-  // var item = req.body.postTitle;
-  // var bodyC = req.body.postBody;
-  // console.log(item);
-  // console.log(bodyC);
   const publish = {
     title: req.body.postTitle,
     content: req.body.postBody
